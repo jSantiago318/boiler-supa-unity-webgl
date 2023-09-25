1a63072118564007c28c12aa30653be572b40fbf
Joshua <joshua.santiago14@upr.edu>
2023-09-24 22:05:44 -0400

    version update

--- src/App.jsx
+++ src/App.jsx
@@ -1,120 +1,116 @@
 import "./index.css";
 
+import { Center, Text } from "@chakra-ui/react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 import { useEffect, useState } from "react";
 
-import { Auth } from "@supabase/auth-ui-react";
-import {Overlay} from "./components/Overlay";
-import { ThemeSupa } from "@supabase/auth-ui-shared";
-import { createClient } from "@supabase/supabase-js";
-
-const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
-);
-
-
+import { Authentication } from "../screens/Authentication";
+import { Overlay } from "./components/Overlay";
+import { supabase } from "./supabaseClient";
 
 export default function App() {
   const [session, setSession] = useState(null);
-
+  // change device pixel ratio when resizing the browser window
   const [devicePixelRatio, setDevicePixelRatio] = useState(
     window.devicePixelRatio
   );
 
-  const { unityProvider, loadingProgression, isLoaded } = useUnityContext({
-    loaderUrl: "/Builds.loader.js",
-    dataUrl: "/Builds.data.br",
-    frameworkUrl: "/Builds.framework.js.br",
-    codeUrl: "/Builds.wasm.br",
-  });
-
-
-
-
-
-
+  // following files are from the Unity build exporter for webgl
+  const { unityProvider, loadingProgression, isLoaded, sendMessage } =
+    useUnityContext({
+      loaderUrl: "/Builds.loader.js",
+      dataUrl: "/Builds.data.br",
+      frameworkUrl: "/Builds.framework.js.br",
+      codeUrl: "/Builds.wasm.br",
+    });
 
   useEffect(() => {
-
-
-
+    // We'll use the onAuthStateChange() method to listen for changes in
+    // authentication state (e.g. signing in, signing out, etc.)
     supabase.auth.getSession().then(({ data: { session } }) => {
+      // on build check if session has a registered user
       setSession(session);
     });
 
     const {
       data: { subscription },
     } = supabase.auth.onAuthStateChange((_event, session) => {
+      // while on webpage check if user has authenticated
       setSession(session);
     });
 
-
-
-
-
-
-    
-       // A function which will update the device pixel ratio of the Unity
-      // Application to match the device pixel ratio of the browser.
-      const updateDevicePixelRatio = function () {
-        setDevicePixelRatio(window.devicePixelRatio);
-      };
-      // A media matcher which watches for changes in the device pixel ratio.
-      const mediaMatcher = window.matchMedia(
-        `screen and (resolution: ${devicePixelRatio}dppx)`
-      );
-      // Adding an event listener to the media matcher which will update the
-      // device pixel ratio of the Unity Application when the device pixel
-      // ratio changes.
-      mediaMatcher.addEventListener("change", updateDevicePixelRatio);
-      return function () {
-        // Removing the event listener when the component unmounts.
-        mediaMatcher.removeEventListener("change", updateDevicePixelRatio);
-      };
-
-    return () => subscription.unsubscribe();
+    // A function which will update the device pixel ratio of the Unity
+    // Application to match the device pixel ratio of the browser.
+    const updateDevicePixelRatio = function () {
+      setDevicePixelRatio(window.devicePixelRatio);
+    };
+    // A media matcher which watches for changes in the device pixel ratio.
+    const mediaMatcher = window.matchMedia(
+      `screen and (resolution: ${devicePixelRatio}dppx)`
+    );
+    // Adding an event listener to the media matcher which will update the
+    // device pixel ratio of the Unity Application when the device pixel
+    // ratio changes.
+    mediaMatcher.addEventListener("change", updateDevicePixelRatio);
+    return function () {
+      // Removing the event listener when the component unmounts.
+      mediaMatcher.removeEventListener("change", updateDevicePixelRatio);
+    };
   }, [devicePixelRatio]);
-
-
-
-
-
-
 
   const loadingPercentage = Math.round(loadingProgression * 100);
   if (!session) {
-    return <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />;
+    // if user has no active session redirect to user authentication
+    return <Authentication />;
   } else {
-
-
-
-
-
-
-
-
-
-    
     return (
-    <>
-        {isLoaded === false && (
-        // We'll conditionally render the loading overlay if the Unity
-        // Application is not loaded.
-        <div className="loading-overlay">
-          <p>Loading... ({loadingPercentage}%)</p>
-        </div>
-      )}
-      
-        <Overlay/>
-      <Unity
-        unityProvider={unityProvider}
-        devicePixelRatio={devicePixelRatio}
-        style={{ visibility: isLoaded ? "visible" : "hidden", width: "100%", height: "100%", top: "0", left: "0", position: "absolute" }}
-      />
-    </>
+      <>
+        {isLoaded === false ? (
+          // We'll conditionally render the loading overlay if the Unity
+          // Application is not loaded.
+          <Center
+            position="absolute"
+            width="100%"
+            height="100%"
+            top="0"
+            left="0"
+            backgroundColor="black"
+          >
+            <Text
+              color="white"
+              fontSize="xl"
+              fontWeight="bold"
+              textAlign="center"
+            >
+              Loading... ({loadingPercentage}%)
+            </Text>
+          </Center>
+        ) : (
+          //Overlay shows buttons on top of Unity webgl
+          //Example button:
+          // - Sign Out
+          // - Show Email inside of Unity Text Object
 
-        
+          <Overlay
+            session={session}
+            sendMessage={sendMessage}
+            unityProvider={unityProvider}
+          />
+        )}
+
+        <Unity
+          unityProvider={unityProvider}
+          devicePixelRatio={devicePixelRatio}
+          style={{
+            visibility: isLoaded ? "visible" : "hidden",
+            width: "100%",
+            height: "100%",
+            top: "0",
+            left: "0",
+            position: "absolute",
+          }}
+        />
+      </>
     );
   }
 }
--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -1,22 +1,93 @@
-import { Button, Stack } from "@chakra-ui/react";
+import { Button, Stack, Input } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import { supabase } from "../supabaseClient";
+import Sidebar from "./Sidebar";
 
-import React from "react";
-import {supabase} from "../supabaseClient"
+export const Overlay = (
+  {sendMessage}
+) => {
+  // set email to null to prevent infinite loop
+  const [email, setEmail] = React.useState(null);
+  const [searchQuery, setSearchQuery] = useState("");
 
-export const Overlay = () => {
-  return (<Stack style={styles.overlay}>
-     <Button
-        >
-            UPRM Map
-        </Button>
-        <Button
-        onClick={async ()=> {
-            await supabase.auth.signOut()
+  useEffect(
+    () => {
+      if (email === null) {
+        supabase.auth.getSession().then(({ data: { session } }) => {
+          // on build check if session has a registered user
+          setEmail(session.user.email);
+        });
+      }
+    },
+    // run tick to check if email is null and get email from session
+    [email]
+  );
+
+  function showUserEmail() {
+    // Function is called when button is clicked
+    // emailText is the name of the GameObject in Unity
+    // FetchEmail is the name of the function in Unity
+    // email is the parameter passed to the function in Unity
+    if (email !== null) {
+      sendMessage("emailText", "FetchEmail", email);
+    } else {
+      return "Loading...";
+    }
+  }
+  function handleSearch() {
+    // Handle search here (you can call a function or perform an API request)
+    console.log("Search Query:", searchQuery);
+  }
+  return (
+    // horizontal stack of buttons
+
+    <>
+
+      <Stack
+        direction={"col"}
+        spacing={"100%"}
+        height={"100%"}
+        width={"100%"}
+        style={styles.sidebar}
+      >
+        <Sidebar />
+      </Stack>
+
+
+    <Stack
+      direction={"row"}
+      spacing={"5%"}
+      height={"100%"}
+      width={"100%"}
+      style={styles.overlay}
+    >
+      {/* <Button>UPRM Interactive Map</Button> */}
+      <Button><img style={{width: 230, height: 50}} src="https://i.ibb.co/3sB25sy/Logo-removebg-preview.png" /></Button>
+      <Button
+        onClick={() => {
+          showUserEmail();
         }}
-        >
-            Sign Out
-        </Button>
-  </Stack>)
+      >
+        Profile
+      </Button>
+      <Input
+        type="text"
+        placeholder="Search..."
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      >
+      </Input>
+      <Button onClick={() => handleSearch()}>Search</Button>
+      <Button
+        onClick={() => {
+          supabase.auth.signOut();
+        }}
+      >
+        Sign Out
+      </Button>
+          </Stack>
+    </>
+  );
 };
 
 const styles = {
@@ -28,4 +99,22 @@ const styles = {
     height: "10%",
     zIndex: "1",
   },
+  btn: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: "black",
+    color: "white",
+    fontSize: "1.5vw",
+    fontWeight: "bold",
+    border: "none",
+    outline: "none",
+  },
+  sidebar: {
+    position: "absolute",
+    top: "15%",
+    left: "5%",
+    width: "10%",
+    height: "10%",
+    zIndex: "1",
+  }
 };
